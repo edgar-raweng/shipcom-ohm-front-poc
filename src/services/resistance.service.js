098285e919edd3c2fc6bafefda41d2d50e671665
Yanely <yannyyss@hotmail.com>
2021-06-15 12:31:59 -0500

    response data adjusted

--- src/services/resistance.service.js
+++ src/services/resistance.service.js
@@ -5,7 +5,7 @@ const url = 'https://shipcomtest.azurewebsites.net/api/ohm?'
 export async function getResistanceOHMSValueService (firstBand, secondBand, multiplierBand, toleranceBrand) {
     try {
         const result = await axios.get(`${url}bandAColor=${firstBand}&bandBColor=${secondBand}&bandCColor=${multiplierBand}&bandDColor=${toleranceBrand}`)
-        return result
+        return result.data.result
     } catch(e) {
         console.log('Error',e)
     }
